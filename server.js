c185289cb14b11de0a6ef61703c682aed7c8aedd
Naze Zapata <nathaniel.zapata@gmail.com>
2019-03-25 16:27:42 -0400

    bla bla
    bla

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/v1', require('./routes/api'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404));ad a
 });
 
 // error handler
